c31ac8711041554217bd787cbb4455bb31dea5e7
manreetgill <reetg311@gmail.com>
2025-03-08 12:49:42 -0500

    Co-authored-by: audreejacob <audreejacob@users.noreply.github.com> Co-authored-by: dishsoapie <dishsoapie@users.noreply.github.com>

--- hackher-project.py
+++ hackher-project.py
@@ -1,1 +1,19 @@
+""""
+- Main menu 
+    1. Title 
+    2. Welcoming message
+    3. Sign in options (2 Buttons)
 
+"""
+#im gonna watch the women in tech panel on teams fr 
+
+def main():
+    print("Hello! Welcome to Frolick Finder! This platform will allow you to find local events. We're here to save your time and money!")
+    print("Sign in as an event organizer")
+    print("Sign in as a member")
+    
+    user_inp = input("What would you like to do?")
+
+    if user_inp == "Sign in as an event organizer":
+    
+#can i do something yes one sec
